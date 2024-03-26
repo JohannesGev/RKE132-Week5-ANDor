bba26dff8e08fdc361c58c833a0365c68ead119c
JohannesG <johannes.gevlja@tktk.ee>
2024-03-26 13:57:16 +0200

    add week 6 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.ComponentModel.Design;
+using System.Threading.Channels;
+
+int math, biology, chemestry;
+
+Console.WriteLine("Enter your Math result:") ;
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemestry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemestry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
+
+     
